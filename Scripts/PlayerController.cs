f50af4583286e9d2bde8a02b4c748cb0b2cfd9e3
wqferr <wqferr@gmail.com>
2016-10-28 19:31:31 -0200

    PlayerController v1

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 0;
+
+    private SpriteRenderer sr;
+
+    void Awake() {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        Vector3 moveDir = new Vector3(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"),
+            0
+        ).normalized;
+
+        transfrom.Translate(moveDir * speed);
+        // TODO update sprite
+    }
+
+}
