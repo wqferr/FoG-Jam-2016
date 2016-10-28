5a1e6a5a4a542e540fec3d0a9755d34abdffe7ae
wqferr <wqferr@gmail.com>
2016-10-28 19:55:11 -0200

    Consertar codigo de translação da camera

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     private Vector3 relativePos = null;
+    private GameObject player = null;
 
     void Awake() {
         PlayerController pc =
                 (PlayerController) FindObjectOfType(typeof(PlayerController));
 
         if (pc) {
-            GameObject player = pc.gameObject;
+            player = pc.gameObject;
             relativePos = transform.position - player.transform.position;
         } else {
             Debug.Log("Não tem jogador nessa porra.");
@@ -20,6 +21,6 @@ public class CameraController : MonoBehaviour {
     }
 
     void Update() {
-        transform.Translate(relativePos);
+        transform.position = player.transform.position + relativePos;
     }
 }
