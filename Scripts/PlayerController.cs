d7ea93e0bf5227cc88023e6da28785a78a7180c0
wqferr <wqferr@gmail.com>
2016-10-28 23:30:49 -0200

    Change player sprite based on input direction

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public const int SPR_U = 0;
+    public const int SPR_UR = 1;
+    public const int SPR_R = 2;
+    public const int SPR_DR = 3;
+    public const int SPR_D = 4;
+
     public float speed = 0;
+
+    public Sprite[] sprites = new Sprite[5];
 
     private SpriteRenderer sr;
 
@@ -21,6 +29,30 @@ public class PlayerController : MonoBehaviour {
 
         transfrom.Translate(moveDir * speed);
         // TODO update sprite
+    }
+
+    private void updateSprites(Vector3 moveDir) {
+        if (moveDir.x == 0) {
+            // Stopped horizontally
+            if (moveDir.y > 0) {
+                sr.sprite = sprites[SPR_U];
+            } else if (moveDir.y < 0) {
+                sr.sprite = sprites[SPR_D];
+            }
+        } else {
+            // Moving horizontally
+            if (moveDir.y > 0) {
+                sr.sprite = sprites[SPR_UR];
+            } else if (moveDir.y < 0) {
+                sr.sprite = sprites[SPR_DR];
+            }
+
+            if (moveDir.x < 0) {
+                sr.flipX = true;
+            } else {
+                sr.flipX = false;
+            }
+        }
     }
 
 }
